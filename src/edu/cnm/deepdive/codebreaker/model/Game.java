f3127e5fc22799ed23bfafcda769fafc14955bec
rfrech <rfrechette@cnm.edu>
2020-09-25 09:07:05 -0700

    added outputs

--- src/edu/cnm/deepdive/codebreaker/model/Game.java
+++ src/edu/cnm/deepdive/codebreaker/model/Game.java
@@ -9,6 +9,8 @@ import java.util.Random;
 public class Game {
 
   private static final String BAD_GUESS_PATTERN_FORMAT = "^.*[^%s].*$";
+  private static final String ILLEGAL_LENGTH_MESSAGE = "Invalid guess length: required=%d; provided=%d.";
+  private static final String ILLEGAL_CHARACTER_MESSAGE = "Guess includes invalid characters: required=%s; provided=%s.";
 
   private final Code code;
   private final List<Guess> guesses;
@@ -17,7 +19,7 @@ public class Game {
   private final String badGuessPattern;
 
   public Game(String pool, int length, Random rng) {
-    code = new Code (pool, length, rng);
+    code = new Code(pool, length, rng);
     guesses = new LinkedList<>();
     this.pool = pool;
     this.length = length;
@@ -42,10 +44,12 @@ public class Game {
 
   public Guess guess(String text) {
     if (text.length() != length) {
-      throw new IllegalArgumentException();
+      throw new IllegalArgumentException(String.format(
+          ILLEGAL_LENGTH_MESSAGE, length, text.length()));
     }
     if (text.matches(badGuessPattern)) {
-      throw new IllegalArgumentException();
+      throw new IllegalArgumentException(String.format(
+          ILLEGAL_CHARACTER_MESSAGE, pool, text));
     }
     Guess guess = code.new Guess(text);
     guesses.add(guess);
